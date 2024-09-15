9796b5d576411cca28235040b1a2eff13922ed57
LeonelCledon <166616600+LeonelCledon@users.noreply.github.com>
2024-09-15 16:16:39 -0300

    Update Ej1.cpp

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include "Cola.h"
+
 using namespace std;
 
-int main() {
+bool sonIguales(Cola<char> &cola1, Cola<char> &cola2){
+    while (!cola1.esVacia() && !cola2.esVacia()){
+        if (cola1.peek() != cola2.peek()){
+            return false;
+        }
+        cola1.desencolar();
+        cola2.desencolar();
+    }
+    return cola1.esVacia() && cola2.esVacia();
+}
 
-    cout << "Ejercicio N° 1" << endl;
+int main(){
+    Cola<char> cola1;
+    Cola<char> cola2;
 
-    return 0;
+    cout << "Ingrese caracteres para la primera cola (ingrese '.' para terminar): " << endl;
+    char input;
+    while (cin >> input && input != '.') {
+        cola1.encolar(input);
+    }
+
+    cout << "Ingrese caracteres para la segunda cola (ingrese '.' para terminar): " << endl;
+    while (cin >> input && input != '.') {
+        cola2.encolar(input);
+    }
+
+    if (sonIguales(cola1, cola2)) {
+        cout << "Las colas son iguales" << endl;
+    } else {
+        cout << "Las colas son diferentes" << endl;
+    }
 }
