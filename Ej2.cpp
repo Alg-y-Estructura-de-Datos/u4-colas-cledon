39ccf66727def3fca03710cd2ee4a954a3cfd5d2
LeonelCledon <166616600+LeonelCledon@users.noreply.github.com>
2024-09-15 16:24:23 -0300

    Update Ej2.cpp

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include "Cola.h"
+
 using namespace std;
 
-int main() {
+void eliminarMayor(Cola<int> &cola, int limite) {
+    Cola<int> colaAux;
 
-    cout << "Ejercicio N° 2" << endl;
+    while (!cola.esVacia()) {
+        int dato = cola.desencolar();
+        if (dato <= limite) {
+            colaAux.encolar(dato);
+        }
+    }
+
+    while (!colaAux.esVacia()) {
+        cola.encolar(colaAux.desencolar());
+    }
+}
+
+int main() {
+    Cola<int> cola;
+    int limite;
+
+    cout << "Ingrese numeros para la cola (ingrese -1 para terminar): " << endl;
+    int input;
+    while (cin >> input && input != -1) {
+        cola.encolar(input);
+    }
+
+    cout << "Ingrese el limite: ";
+    cin >> limite;
+
+    eliminarMayor(cola, limite);
+
+    cout << "Cola depurada: ";
+    while (!cola.esVacia()) {
+        cout << cola.desencolar() << " ";
+    }
+    cout << endl;
 
     return 0;
 }
